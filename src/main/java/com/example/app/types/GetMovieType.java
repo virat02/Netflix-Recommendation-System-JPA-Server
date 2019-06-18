a08f88e28468bbf9f4a7013fcca2fb81483d8d5e
Shaurya Carvalho <carvalho.sh@husky.neu.edu>
2019-06-18 19:10:36 -0400

    added movie services

---
+++ src/main/java/com/example/app/types/GetMovieType.java
@@ -0,0 +1,21 @@
+package com.example.app.types;
+
+public enum GetMovieType {
+
+    POPULAR("popular"),
+    TOP_RATED("top_rated"),
+    UPCOMING("upcoming"),
+    NOW_PLAYING("now_playing"),
+    SEARCH("search");
+
+    private final String getMovieType;
+
+    private GetMovieType(String getMovieType) {
+        this.getMovieType = getMovieType;
+    }
+
+    @Override
+    public String toString() {
+        return getMovieType;
+    }
+}
